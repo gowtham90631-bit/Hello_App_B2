4ea053b12615e377287222fd64c980d8c70b2243
gowtham90631-bit <gowtham90631@gmail.com>
2026-03-25 08:39:13 +0530

    first commit

--- Helloapp.java
+++ Helloapp.java
@@ -1,18 +1,12 @@
-public class Helloapp{
-    public static void main (String[] args){
-        StringBuilder nameBuilder = new StringBuilder();
-        for (int i = 0; i < args.length; i++){
-            nameBuilder.append(args[i]);
-            if (i < args.length - 1){
-                nameBuilder.append(", ");
+public class Helloapp {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            for (String name : args) {   // enhanced for loop
+                System.out.println("Hello, " + name + "!");
             }
         }
-        if (args.length == 0){
-        System.out.println("Hello, World!");
-        return;
-        }
-        String name = nameBuilder.toString();
-        System.out.println("Hello " + name + "!");
     }
 }
-	
