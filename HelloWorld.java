04a16a69bf1852963dfd9caa9e6cfb89bd0587c2
gowtham90631-bit <gowtham90631@gmail.com>
2026-04-21 09:08:33 +0530

    UC7: Greeting using String.join()

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,24 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-<<<<<<< HEAD
+        // Default case
         if (args.length == 0) {
             System.out.println("Hello, World!");
             return;
         }
-=======
-        String name = (args.length > 0) ? args[0] : "World";
 
-        System.out.println("Hello " + name);
->>>>>>> 5b3b45e61829c16987dded9d4622b342c9bd86bb
-
-        String names = "";
-
-        for (String name : args) {
-            names += name + ", ";
-        }
-
-        names = names.substring(0, names.length() - 2);
+        // Join names with comma
+        String names = String.join(", ", args);
 
         System.out.println("Hello, " + names + "!");
     }
